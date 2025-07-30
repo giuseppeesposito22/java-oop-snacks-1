65fdf766c7136581450c64c066db1d9c291ea13d
giuseppeesposito22 <gius.espo22@gmail.com>
2025-07-30 11:21:54 +0200

    add class Main

---
+++ src/org/lessons/java/snack/Main.java
@@ -0,0 +1,15 @@
+package org.lessons.java.snack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Studente studente1 = new Studente("Marrio", "Rossi", 30);
+
+        System.out.println(studente1.datiStudente());
+
+
+
+
+    }
+    
+}
